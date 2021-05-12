8c344bd1e9f8e0bd00993eee80bea994135ea9cc
Scorpion309 <Tustin@tut.by>
2021-05-12 20:33:20 +0300

    Add validator and fix code

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-import json
-
-from loc import Location
 from pydantic import BaseModel, validator
+from loc import Location
+
 
 class Temperature(BaseModel):
     temp: int
     feels_like: float
     temp_min: int
     temp_max: int
+    @validator('temp')
+    def to_celsius(cls, v):
+        return int(v*1.8+32)
 
 
 class Weather(BaseModel):
@@ -17,20 +19,26 @@ class Weather(BaseModel):
     name: str
 
 
-city = Location(25, 60)
-weather_info = city.get_info()
-
-weather = {
-            'temperature': {
-                            'temp': weather_info['main']['temp'],
-                            'feels_like': weather_info['main']['feels_like'],
-                            'temp_min': weather_info['main']['temp_min'],
-                            'temp_max': weather_info['main']['temp_max']
+def main(response):
+    return{
+                'temperature': {
+                                'temp': response['main']['temp'],
+                                'feels_like': response['main']['feels_like'],
+                                'temp_min': response['main']['temp_min'],
+                                'temp_max': response['main']['temp_max']
                             },
-            'pressure': weather_info['main']['pressure'],
-            'description': weather_info['weather'][0]['description'],
-            'name': weather_info['name']
-        }
+                'pressure': response['main']['pressure'],
+                'description': response['weather'][0]['description'],
+                'name': response['name']
+            }
 
-info = Weather.parse_raw(json.dumps(weather))
-print(info)
+
+if __name__ == '__main__':
+    city = Location(25, 60)
+    weather_info = city.get_info()
+    try:
+        weather = main(weather_info)
+        info = Weather(**weather)
+        print(info)
+    except:
+        print('Ошибка получения и обработки данных!!!')
