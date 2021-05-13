8b4a94eed6768a5cfffd929b924ce19dda8977cd
Scorpion309 <Tustin@tut.by>
2021-05-13 19:13:01 +0300

    Fix function main(). Fix PEP. Fix error message.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, validator
+
 from loc import Location
 
 
@@ -7,9 +8,10 @@ class Temperature(BaseModel):
     feels_like: float
     temp_min: int
     temp_max: int
+
     @validator('temp')
     def to_fahrenheit(cls, v):
-        return int(v*1.8+32)
+        return int(v * 1.8 + 32)
 
 
 class Weather(BaseModel):
@@ -19,26 +21,26 @@ class Weather(BaseModel):
     name: str
 
 
-def main(response):
-    return{
-                'temperature': {
-                                'temp': response['main']['temp'],
-                                'feels_like': response['main']['feels_like'],
-                                'temp_min': response['main']['temp_min'],
-                                'temp_max': response['main']['temp_max']
-                            },
-                'pressure': response['main']['pressure'],
-                'description': response['weather'][0]['description'],
-                'name': response['name']
-            }
-
-
-if __name__ == '__main__':
+def main():
     city = Location(25, 60)
     weather_info = city.get_info()
     try:
-        weather = main(weather_info)
+        weather = {
+            'temperature': {
+                'temp': weather_info['main']['temp'],
+                'feels_like': weather_info['main']['feels_like'],
+                'temp_min': weather_info['main']['temp_min'],
+                'temp_max': weather_info['main']['temp_max']
+            },
+            'pressure': weather_info['main']['pressure'],
+            'description': weather_info['weather'][0]['description'],
+            'name': weather_info['name']
+        }
         info = Weather(**weather)
         print(info)
-    except:
-        print('Ошибка получения и обработки данных!!!')
+    except KeyError:
+        print('Error! Some value is empty!')
+
+
+if __name__ == '__main__':
+    main()
